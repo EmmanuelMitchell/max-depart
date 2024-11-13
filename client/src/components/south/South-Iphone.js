aabca9f4f3473edebe31125605d1076be19b283a
Musa Enoch Ngawuja <93747533+Enoch134@users.noreply.github.com>
2024-11-13 20:12:54 +0000

    Update South-Iphone.js

--- client/src/components/south/South-Iphone.js
+++ client/src/components/south/South-Iphone.js
@@ -8,7 +8,7 @@ function SouthIphone() {
   const createdAt = new Date().toISOString();
 
   fetch(
-    `https://qrcodes-app-runt.vercel.app/api/maxitapp-for-south/Iphone`,
+    `https://maxit-app.vercel.app/api/maxitapp-for-south/Iphone`,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
