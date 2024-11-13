ec70a1f17bfa800321f00fcd7c38af8fa7097a91
Musa Enoch Ngawuja <93747533+Enoch134@users.noreply.github.com>
2024-11-13 20:12:31 +0000

    Update South-Android.js

--- client/src/components/south/South-Android.js
+++ client/src/components/south/South-Android.js
@@ -7,7 +7,7 @@ function SouthAndroid() {
   const createdAt = new Date().toISOString();
 
   fetch(
-    `https://qrcodes-app-runt.vercel.app/api/maxitapp-for-south/Android`,
+    `https://maxit-app.vercel.app/api/maxitapp-for-south/Android`,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
