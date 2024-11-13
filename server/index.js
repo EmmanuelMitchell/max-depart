88fb261673e6cbd1732c7ea633614476e440f9b1
Musa Enoch Ngawuja <93747533+Enoch134@users.noreply.github.com>
2024-11-13 19:14:30 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(bodyParser.json());
 
 dotenv.config()
-const PORT = process.env.PORT || 4500;
+const PORT = process.env.PORT || 4600;
 
 app.use(cors());
 app.use(express.json());
